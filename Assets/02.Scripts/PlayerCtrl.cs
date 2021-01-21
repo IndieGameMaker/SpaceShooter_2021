1babdbe8dca96206921ebb54f3a575151a73cef5
Lee JaeHyun <myleje@gmail.com>
2021-01-21 17:46:23 +0900

    .

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class PlayerCtrl : MonoBehaviour
 {
+    public float moveSpeed = 6.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +28,7 @@ public class PlayerCtrl : MonoBehaviour
 
         Vector3 moveDir = (Vector3.forward * v) + (Vector3.right * h);
         
-        transform.Translate(moveDir * 0.1f);
+        transform.Translate(moveDir.normalized * Time.deltaTime *  moveSpeed);
 
         // transform.Translate(Vector3.forward * 0.1f * v);  //Vector3.forward = new Vector3(0, 0, 1)
         // transform.Translate(Vector3.right * 0.1f * h);
